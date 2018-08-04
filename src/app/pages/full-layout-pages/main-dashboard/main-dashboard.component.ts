38c8575e41c4722601eb54da057f6638b32785df
Raven Duran <rabintoy@gmail.com>
2018-08-04 17:24:03 +0800

    Random Pokemon feature

--- src/app/pages/full-layout-pages/main-dashboard/main-dashboard.component.ts
+++ src/app/pages/full-layout-pages/main-dashboard/main-dashboard.component.ts
@@ -6,13 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-dashboard.component.scss']
 })
 export class MainDashboardComponent {
+  pokemonSrc: string[];
   pokemonName: string;
+  currentPokemonShown: string;
 
   constructor() {
-
+    this.pokemonSrc = ["Celebi", "Ditto", "Eevee", "Pikachu"];
+    this.currentPokemonShown = this.getRandomPokemonName();
   }
 
-  onSubmit() {
-    alert();
+  getPokemonImageAsset(pokemonName): string {
+    return `assets/img/pokemon/${pokemonName}.jpg`;
+  }
+
+  getRandomPokemonName(): string {
+    return this.pokemonSrc[Math.floor(Math.random() * 100 % this.pokemonSrc.length)];
+  }
+
+  onSubmit(pokemonName) {
+    if (pokemonName === this.currentPokemonShown) {
+      alert("Correct! It's " + pokemonName + "!");
+      this.currentPokemonShown = this.getRandomPokemonName();
+    } else {
+      alert("Wrong! Try again");
+    }
+    this.pokemonName = "";
   }
 }
